3e03d82f71d8c4d921dbd15c560b006947e42aaa
dltmddus <tmddus4671@gmail.com>
2025-01-16 12:41:50 +0900

    init: spring 프로젝트 초기 설정 및 gitignore 추가

---
+++ backend/box-it/src/main/java/com/box_it/box_it/BoxItApplication.java
@@ -0,0 +1,13 @@
+package com.box_it.box_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxItApplication.class, args);
+	}
+
+}
---
+++ backend/box-it/src/test/java/com/box_it/box_it/BoxItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.box_it.box_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
