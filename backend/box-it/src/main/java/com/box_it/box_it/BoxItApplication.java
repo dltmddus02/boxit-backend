acb7c217b01435405d151c235aa121495641a7b2
dltmddus <tmddus4671@gmail.com>
2025-01-22 13:18:10 +0900

    fix: JPA 엔터티 생성/수정 시간 자동 관리 위한 @EnableJpaAuditing 추가

--- backend/box-it/src/main/java/com/box_it/box_it/BoxItApplication.java
+++ backend/box-it/src/main/java/com/box_it/box_it/BoxItApplication.java
@@ -2,12 +2,14 @@ package com.box_it.box_it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoxItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoxItApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoxItApplication.class, args);
+    }
 
 }
