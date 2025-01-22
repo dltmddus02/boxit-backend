072c221eb6193d7a312e001083163e9beaa9e2cd
dltmddus <tmddus4671@gmail.com>
2025-01-22 13:14:12 +0900

    fix: MemberRepository가 JpaRepository 상속 받도록 수정

--- backend/box-it/src/main/java/com/box_it/box_it/repository/MemberRepository.java
+++ backend/box-it/src/main/java/com/box_it/box_it/repository/MemberRepository.java
@@ -3,8 +3,9 @@ package com.box_it.box_it.repository;
 import com.box_it.box_it.domain.Member;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Member save(Member member);
 
     Optional<Member> findById(Long id);
