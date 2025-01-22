eb57a511aa52be2a4353ba4a84b6050b077162f1
dltmddus <tmddus4671@gmail.com>
2025-01-22 13:12:22 +0900

    fix: Member 클래스 @Getter, @Setter, @Entity 어노테이션 추가

--- backend/box-it/src/main/java/com/box_it/box_it/domain/Member.java
+++ backend/box-it/src/main/java/com/box_it/box_it/domain/Member.java
@@ -1,23 +1,83 @@
 package com.box_it.box_it.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    private String password;
+
     private String nickname;
+
     private String profileImageUrl;
+
     private String intro;
+
     private LocalDateTime joinDate;
 
     public Member() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getNickname() {
+//        return nickname;
+//    }
+//
+//    public String getProfileImageUrl() {
+//        return profileImageUrl;
+//    }
+//
+//    public String getIntro() {
+//        return intro;
+//    }
+//
+//    public LocalDateTime getJoinDate() {
+//        return joinDate;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setNickname(String nickname) {
+//        this.nickname = nickname;
+//    }
+//
+//    public void setProfileImageUrl(String profileImageUrl) {
+//        this.profileImageUrl = profileImageUrl;
+//    }
+//
+//    public void setIntro(String intro) {
+//        this.intro = intro;
+//    }
+//
+//    public void setJoinDate(LocalDateTime joinDate) {
+//        this.joinDate = joinDate;
+//    }
 }
