b33d71c116ef67984f93c54f226fb1b540991734
dltmddus <tmddus4671@gmail.com>
2025-02-08 17:58:48 +0900

    fix: 비밀번호 암호화 위한 passwordEncoder 추가

--- backend/box-it/src/main/java/com/box_it/box_it/service/MemberService.java
+++ backend/box-it/src/main/java/com/box_it/box_it/service/MemberService.java
@@ -5,12 +5,14 @@ import com.box_it.box_it.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
 //    public MemberService(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
@@ -26,6 +28,9 @@ public class MemberService {
     public Long join(String username, String password) {
         Member member = new Member();
         member.setName(username);
+
+        String encodedPassword = passwordEncoder.encode(password);
+        member.setPassword(encodedPassword);
 
         validateDuplicateMember(member);
 
